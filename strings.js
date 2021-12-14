f2b6190fb6861699f0ffdf6720df6db6bf93d4e9
mounika <mounikamusham12@gmail.com>
2021-12-14 18:25:11 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,56 @@
+
+var myStr="helloo everyone good afternoon";
+console.log(myStr.length);
+
+//
+
+var myStr="helloo everyone good afternoon";
+console.log(myStr.indexOf('good'));
+
+//
+var myStr="helloo everyone good afternoon";
+console.log(myStr.lastIndexOf('good'));
+
+//
+
+var str="helloo everyone good afternoon";
+console.log(str.slice(0,7));
+console.log(str.slice(-10));
+console.log(str.slice(8));
+
+//
+var str="helloo everyone good afternoon";
+console.log(str.substring(5,9));
+
+//
+
+var str="helloo everyone good afternoon";
+console.log(str.toUpperCase(myStr));
+
+//
+
+var str="FULL STACK DEVELOPER";
+console.log(str.toLowerCase(myStr));
+
+// 
+
+var firstStr="good";
+var secondStr="afternoon";
+console.log(firstStr.concat(' ',secondStr));
+console.log(firstStr.concat(secondStr));
+console.log(firstStr.concat('@',secondStr));
+
+//
+
+var myStr="     Hello    ";
+console.log(myStr.trim());
+
+//
+var str="FULL STACK DEVELOPER";
+console.log(str.charAt(5));
+
+//
+var str="FULL STACK DEVELOPER";
+console.log(str.split(''));
+console.log(str.split(" "));
+console.log(str.split(','));
